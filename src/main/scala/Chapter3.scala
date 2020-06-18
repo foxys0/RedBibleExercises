5530a9ff198693cb5f81f842b731042659d8ad82
cerny.matej <cerny.matej@gmail.com>
2020-06-18 16:36:15 +0200

    Chapter 3: tail, setHead, drop

--- src/main/scala/Chapter3.scala
+++ src/main/scala/Chapter3.scala
@@ -1,10 +1,13 @@
+import annotation.tailrec
+
 object Chapter3 {
 
   sealed trait MyList[+A]
-  case object Nil extends MyList[Nothing]
-  case class Cons[+A](head: A, tail: MyList[A]) extends MyList[A]
 
   object MyList {
+    case object Nil extends MyList[Nothing]
+    case class Cons[+A](head: A, tail: MyList[A]) extends MyList[A]
+
     def apply[A](as: A*): MyList[A] = if (as.isEmpty) Nil else Cons(as.head, apply(as.tail: _*))
     def sum(ints: MyList[Int]): Int = ints match {
       case Nil => 0
@@ -23,6 +26,30 @@ object Chapter3 {
       case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
       case Cons(h, t) => h + sum(t)
       case _ => 101
+    }
+
+    /** Exercise 2: Removes first element of list */
+    def tail[A](myList: MyList[A]): Option[MyList[A]] = myList match {
+      case Nil => None
+      case Cons(_, tail) => Some(tail)
+    }
+
+    /** Exercise 3: Replaces first element of list */
+    def setHead[A](head: A, myList: MyList[A]): Option[MyList[A]] = myList match {
+      case Nil => None
+      case Cons(_, tail) => Some(Cons(head, tail))
+    }
+
+    /** Exercise 4: Drops first n elements of list */
+    def drop[A](myList: MyList[A], n: Int): MyList[A] = {
+      @tailrec
+      def loop(xs: MyList[A], i: Int): MyList[A] = {
+        val maybeTail = tail(xs)
+        if (maybeTail.isEmpty | i == 0) xs
+        else loop(maybeTail.get, i - 1)
+      }
+
+      loop(myList, n)
     }
   }
 
--- src/test/scala/Chapter3Test.scala
+++ src/test/scala/Chapter3Test.scala
@@ -1,5 +1,5 @@
 import Chapter3.MyList
-import Chapter3.MyList.patternMatching
+import Chapter3.MyList._
 import org.scalatest.funsuite.AnyFunSuite
 
 class Chapter3Test extends AnyFunSuite {
@@ -8,6 +8,28 @@ class Chapter3Test extends AnyFunSuite {
     assert(patternMatching(MyList(1, 2, 3, 4)) == 3)
     assert(patternMatching(MyList()) == 42)
     assert(patternMatching(MyList(1, 2)) == 3)
+  }
+
+  test("tail") {
+    assert(tail(MyList(1, 2, 3)).contains(MyList(2, 3)))
+    assert(tail(MyList("a", "b")).contains(MyList("b")))
+    assert(tail(MyList("a")).contains(Nil))
+    assert(tail(MyList()).isEmpty)
+  }
+
+  test("setHead") {
+    assert(setHead(2, MyList(1, 3, 4)).contains(MyList(2, 3, 4)))
+    assert(setHead("b", MyList("a", "c")).contains(MyList("b", "c")))
+    assert(setHead("b", MyList("a")).contains(MyList("b")))
+    assert(setHead("b", MyList()).isEmpty)
+  }
+
+  test("drop") {
+    assert(drop(MyList(1, 2, 3, 4, 5), 3) == MyList(4, 5))
+    assert(drop(MyList(1, 2), 1) == MyList(2))
+    assert(drop(MyList(1, 2), 3) == MyList())
+    assert(drop(MyList(1), 1) == MyList())
+    assert(drop(MyList(), 1) == MyList())
   }
 
 }
