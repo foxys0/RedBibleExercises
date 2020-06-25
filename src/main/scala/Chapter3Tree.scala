09c9f68b27c4619214826e18931090ab29395ef5
cerny.matej <cerny.matej@gmail.com>
2020-06-25 12:17:19 +0200

    Chapter 3: size, maximum, depth, map, fold

--- src/main/scala/Chapter3Tree.scala
+++ src/main/scala/Chapter3Tree.scala
@@ -6,8 +6,41 @@ object Chapter3Tree {
 
   object Tree {
 
+    /** Exercise 25: Counts all elements in tree */
+    def size[A](tree: Tree[A]): Int = tree match {
+      case Leaf(_) => 1
+      case Branch(left, right) => 1 + size(left) + size(right)
+    }
 
+    /** Exercise 26: Returns maximum value */
+    def maximum(tree: Tree[Int]): Int = tree match {
+      case Leaf(value) => value
+      case Branch(left, right) => maximum(left).max(maximum(right))
+    }
 
+    /** Exercise 27: Returns maximum depth */
+    def depth[A](tree: Tree[A]): Int = tree match {
+      case Leaf(_) => 0
+      case Branch(left, right) => 1 + depth(left).max(depth(right))
+    }
+
+    /** Exercise 28: Map */
+    def map[A, B](tree: Tree[A])(f: A => B): Tree[B] = tree match {
+      case Leaf(value) => Leaf(f(value))
+      case Branch(left, right) => Branch(map(left)(f), map(right)(f))
+    }
+
+    /** Exercise 29: Fold */
+    def fold[A, B](tree: Tree[A])(f: A => B)(g: (B, B) => B): B = tree match {
+      case Leaf(value) => f(value)
+      case Branch(left, right) => g(fold(left)(f)(g), fold(right)(f)(g))
+    }
+
+    def size2[A](tree: Tree[A]): Int = fold(tree)(_ => 1)(1 + _ + _)
+    def maximum2(tree: Tree[Int]): Int = fold(tree)(a => a)(_.max(_))
+    def depth2(tree: Tree[Int]): Int = fold(tree)(_ => 0)(1 + _.max(_))
+    def map2[A, B](tree: Tree[A])(f: A => B): Tree[B] =
+      fold(tree)(a => Leaf(f(a)): Tree[B])(Branch(_, _))
 
   }
 
--- src/test/scala/Chapter3TreeTest.scala
+++ src/test/scala/Chapter3TreeTest.scala
@@ -1,10 +1,51 @@
+import Chapter3Tree._
+import Chapter3Tree.Tree._
 import org.scalatest.funsuite.AnyFunSuite
 
 class Chapter3TreeTest extends AnyFunSuite {
 
-  test("size") {
+  private val one = Leaf(1)
+  private val oneTwo = Branch(Leaf(1), Leaf(2))
+  private val oneTwoThreeFour = Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4)))
 
+  test("size") {
+    assert(size(one) == 1)
+    assert(size(oneTwo) == 3)
+    assert(size(oneTwoThreeFour) == 7)
+    assert(size2(one) == 1)
+    assert(size2(oneTwo) == 3)
+    assert(size2(oneTwoThreeFour) == 7)
   }
 
+  test("maximum") {
+    assert(maximum(one) == 1)
+    assert(maximum(oneTwo) == 2)
+    assert(maximum(oneTwoThreeFour) == 4)
+    val tree = Branch(Branch(Leaf(3), Leaf(2)), Branch(Leaf(4), Leaf(1)))
+    assert(maximum(tree) == 4)
+    assert(maximum2(one) == 1)
+    assert(maximum2(oneTwo) == 2)
+    assert(maximum2(oneTwoThreeFour) == 4)
+    assert(maximum2(tree) == 4)
+  }
+
+  test("depth") {
+    assert(depth(one) == 0)
+    assert(depth(oneTwo) == 1)
+    assert(depth(oneTwoThreeFour) == 2)
+    val tree = Branch(Leaf(1), Branch(Leaf(2), Branch(Branch(Leaf(4), Leaf(4)), Leaf(3))))
+    assert(depth(tree) == 4)
+    assert(depth2(one) == 0)
+    assert(depth2(oneTwo) == 1)
+    assert(depth2(oneTwoThreeFour) == 2)
+    assert(depth2(tree) == 4)
+  }
+
+  test("map") {
+    assert(map(one)(_ + 1) == Leaf(2))
+    assert(map(oneTwo)(_ + 1) == Branch(Leaf(2), Leaf(3)))
+    assert(map2(one)(_ + 1) == Leaf(2))
+    assert(map2(oneTwo)(_ + 1) == Branch(Leaf(2), Leaf(3)))
+  }
 
 }
