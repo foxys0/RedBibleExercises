821a63af3bba2738303cfe35b3266225ef11f443
cerny.matej <cerny.matej@gmail.com>
2020-07-24 16:53:04 +0200

    Chapter 7: asyncF, sequence, parMap, parFilter

--- src/main/scala/Chapter7Parallelism.scala
+++ src/main/scala/Chapter7Parallelism.scala
@@ -1,17 +1,49 @@
+import java.util.concurrent.{ExecutorService, Future, TimeUnit}
+
 object Chapter7Parallelism {
 
-  type Par[A]
+  type Par[A] = ExecutorService => Future[A]
 
-  def unit[A](a: A): Par[A] = ???
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
+    def get(timeout: Long, units: TimeUnit): A = get
+    def isCancelled = false
+    def cancel(evenIfRunning: Boolean): Boolean = false
+  }
 
-  def fork[A](a: => Par[A]): Par[A] = ???
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
+
+  def unit[A](a: A): Par[A] = (_: ExecutorService) => UnitFuture(a)
 
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
 
-  def run[A](pa: Par[A]): A = ???
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] =
+    (es: ExecutorService) => {
+      val af = a(es)
+      val bf = b(es)
+      UnitFuture(f(af.get, bf.get))
+    }
 
-  def map2[A, B, C](pa: Par[A], pb: Par[B])(f: (A, B) => C): Par[C] =
-    unit(f(run(pa), run(pb)))
+  def fork[A](a: => Par[A]): Par[A] =
+    es => es.submit(() => a(es).get) //new Callable[A] { def call: A = a(es).get }
 
+  /** Exercise 4 */
+  def asyncF[A, B](f: A => B): A => Par[B] = a => lazyUnit(f(a))
+
+  def map[A, B](pa: Par[A])(f: A => B): Par[B] = map2(pa, unit(()))((a, _) => f(a))
+
+  def sortPar(parList: Par[List[Int]]): Par[List[Int]] = map(parList)(_.sorted)
+
+  /** Exercise 5 */
+  def sequence[A](ps: List[Par[A]]): Par[List[A]] = ps match {
+    case pa :: pt => map2(pa, fork(sequence(pt)))(_ :: _)
+    case Nil => unit(Nil)
+  }
+
+  def parMap[A, B](ps: List[A])(f: A => B): Par[List[B]] = sequence(ps.map(asyncF(f)))
+
+  /** Exercise 6 */
+  def parFilter[A](l: List[A])(f: A => Boolean): Par[List[A]] =
+    map(sequence(l.map(asyncF((a: A) => if (f(a)) List(a) else List()))))(_.flatten)
 
 }
