cec38740e27782e630b34cec1c9a53006f75e5f5
cerny.matej <cerny.matej@gmail.com>
2020-08-06 22:22:41 +0200

    Chapter 8: listOf1, choose, sortedProp

--- src/main/scala/Chapter8GenProp.scala
+++ src/main/scala/Chapter8GenProp.scala
@@ -1,23 +1,23 @@
 import Chapter5Stream.Stream
-import Chapter6Random.RNG
+import Chapter6Random.{RNG, SimpleRNG}
 import Chapter6State.State
-import Chapter8GenProp.Prop.{Falsified, Passed, Proved, Result, TestCases}
+import Chapter8GenProp.Prop.{Falsified, MaxSize, Passed, Proved, Result, TestCases}
 
 object Chapter8GenProp {
 
-  case class Prop(run: (TestCases, RNG) => Result) {
+  case class Prop(run: (MaxSize, TestCases, RNG) => Result) {
 
     /** Exercise 9 */
-    def &&(p: Prop): Prop = Prop { (ts, rng) =>
-      run(ts, rng) match {
-        case Passed | Proved => p.run(ts, rng)
+    def &&(p: Prop): Prop = Prop { (max, ts, rng) =>
+      run(max, ts, rng) match {
+        case Passed | Proved => p.run(max, ts, rng)
         case f => f
       }
     }
 
-    def ||(p: Prop): Prop = Prop { (ts, rng) =>
-      run(ts, rng) match {
-        case Falsified(_, _) => p.run(ts, rng)
+    def ||(p: Prop): Prop = Prop { (max, ts, rng) =>
+      run(max, ts, rng) match {
+        case Falsified(_, _) => p.run(max, ts, rng)
         case p => p
       }
     }
@@ -27,6 +27,7 @@ object Chapter8GenProp {
     type FailedCase = String
     type SuccessCount = Int
     type TestCases = Int
+    type MaxSize = Int
 
     sealed trait Result { def isFalsified: Boolean }
     case object Passed extends Result { def isFalsified = false }
@@ -43,7 +44,7 @@ object Chapter8GenProp {
         s"generated an exception: ${e.getMessage}\n" +
         s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
 
-    def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop { (n, rng) =>
+    def forAll[A](as: Gen[A])(f: A => Boolean): Prop = Prop { (_, n, rng) =>
       randomStream(as)(rng)
         .zip(Stream.from(0))
         .take(n)
@@ -56,6 +57,39 @@ object Chapter8GenProp {
         .find(_.isFalsified)
         .getOrElse(Passed)
     }
+
+    def forAll[A](g: SGen[A])(f: A => Boolean): Prop = forAll(g(_))(f)
+
+    def forAll[A](g: Int => Gen[A])(f: A => Boolean): Prop = Prop { (max, n, rng) =>
+      val casesPerSize = (n - 1) / max + 1
+      val props: Stream[Prop] = Stream.from(0).take((n min max) + 1).map(i => forAll(g(i))(f))
+      val prop: Prop = props
+        .map(p =>
+          Prop { (max, _, rng) =>
+            p.run(max, casesPerSize, rng)
+        })
+        .toList
+        .reduce(_ && _)
+
+      prop.run(max, n, rng)
+    }
+
+    def run(
+      p: Prop,
+      maxSize: Int = 100,
+      testCases: Int = 100,
+      rng: RNG = SimpleRNG(System.currentTimeMillis)
+    ): Unit = p.run(maxSize, testCases, rng) match {
+      case Falsified(msg, n) => println(s"! Falsified after $n passed tests:\n $msg")
+      case Passed => println(s"+ OK, passed $testCases tests.")
+      case Proved => println(s"+ OK, proved property.")
+    }
+
+    /** Exercise 14 */
+    val sortedProp: Prop = forAll(Gen.listOf(Gen.choose(-10, 10))) { l =>
+      val ls = l.sorted
+      l.isEmpty || ls.tail.isEmpty || !ls.zip(ls.tail).exists { case (a, b) => a > b }
+    }
   }
 
   case class Gen[A](sample: State[RNG, A]) {
@@ -65,12 +99,17 @@ object Chapter8GenProp {
     /** Exercise 6 */
     def flatMap[B](f: A => Gen[B]): Gen[B] = Gen(sample.flatMap(a => f(a).sample))
     def listOfN(size: Gen[Int]): Gen[List[A]] = size.flatMap(n => Gen.listOfN(n, this))
+    def listOfN(size: Int): Gen[List[A]] = Gen.listOfN(size, this)
 
     /** Exercise 10 */
     def unsized: SGen[A] = SGen(_ => this)
   }
 
   object Gen {
+
+    /** Exercise 4 */
+    def choose(start: Int, stopExclusive: Int): Gen[Int] =
+      Gen(State(RNG.nonNegativeInt).map(n => start + n % (stopExclusive - start)))
 
     /** Exercise 5 */
     def unit[A](a: => A): Gen[A] = Gen(State.unit(a))
@@ -83,11 +122,16 @@ object Chapter8GenProp {
     def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] =
       boolean.flatMap(b => if (b) g1 else g2)
 
+    /** Exercise 12 */
+    def listOf[A](g: Gen[A]): SGen[List[A]] = SGen(n => g.listOfN(n))
+
+    /** Exercise 13 */
+    def listOf1[A](g: Gen[A]): SGen[List[A]] = SGen(n => g.listOfN(n.max(1)))
   }
 
   case class SGen[A](forSize: Int => Gen[A]) {
 
-    /** Exercise 10 */
+    /** Exercise 11 */
     def apply(n: Int): Gen[A] = forSize(n)
     def map[B](f: A => B): SGen[B] = SGen(forSize(_).map(f))
 
@@ -95,8 +139,7 @@ object Chapter8GenProp {
       val forSize2: Int => Gen[B] = n => forSize(n).flatMap(f(_).forSize(n))
       SGen(forSize2)
     }
+
   }
-
-
 
 }
---
+++ src/test/scala/Chapter8GenPropTest.scala
@@ -0,0 +1,11 @@
+import Chapter6Random.SimpleRNG
+import Chapter8GenProp.{Gen, SGen}
+import org.scalatest.funsuite.AnyFunSuite
+
+class Chapter8GenPropTest extends AnyFunSuite {
+
+  test("dd") {
+
+  }
+
+}
